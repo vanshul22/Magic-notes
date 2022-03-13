dcf0cf21b77c76104dc8ad3a126e57942ee43b0c
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-03-14 01:30:32 +0530

    Added SearchFunction

--- JS/Script.js
+++ JS/Script.js
@@ -3,6 +3,7 @@ Name : Vanshul Kesharwani
 Date : 13/07/2022
 Version : 1.0.0
 Email : vkvanshulkesharwani54@gmail.com
+Further features : Add title, Mark a note as important, Separate notes by user, Add date and time on notes.
 */
 
 // Calling showNotes function to load all notes at starting from local storage.
@@ -58,9 +59,9 @@ function showNotes() {
     notesObj.forEach(function(element, index) {
         // Added html stringVariable to this new generated html stringVariable. for displaying available notes.
         html += `
-        <div class="card mx-4 my-3" style="width: 18rem;">
+        <div class="card noteCard mx-4 my-3" style="width: 18rem;">
             <div class="card-body">
-                <h5 class="card-title">Note ${index+1}</h5>
+                <h5 class="card-title">Note ${index + 1}</h5>
                 <p class="card-text">${element}</p>
                 <button id="${index}" onClick="deleteNote(this.id)" class="btn btn-primary">Delete</button>
             </div>
@@ -81,7 +82,6 @@ function showNotes() {
 
 // Function to delete a note.
 function deleteNote(index) {
-    console.log("ddlelel", index);
     // Taking all local storage in this notes variable.
     let notes = localStorage.getItem("notes");
 
@@ -98,3 +98,31 @@ function deleteNote(index) {
     localStorage.setItem("notes", JSON.stringify(notesObj));
     showNotes();
 };
+
+
+
+// Adding Search function in search area. 
+
+// Targetting search area from ID.
+let searchTxt = document.getElementById("searchTxt");
+// Adding search area to input tag.
+searchTxt.addEventListener("input", function() {
+    // Input taken in this below variable. Convert it into lower case because we need all caps and small letter in search.
+    let inputVal = searchTxt.value.toLowerCase();
+    // Targetting all cards area from Classname.
+    let noteCards = document.getElementsByClassName("noteCard");
+    // make noteCards Array and then looping for all cards.
+    Array.from(noteCards).forEach(function(element) {
+        // Targetting p tag of each element by their tag name.
+        let cardtxt = element.getElementsByTagName("p")[0].innerText;
+
+        // Applying condition and if search text is matching then it will display the card.
+        if (cardtxt.includes(inputVal)) {
+            // console.log(cardtxt);
+            element.style.display = "block";
+        } // Else it will delete/remove the card from page.
+        else {
+            element.style.display = "none";
+        };
+    });
+});
